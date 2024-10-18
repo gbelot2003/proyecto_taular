bcea61dcf9537931b37e74fce2aab437baf3613e
Gerardo Belot <gbelot2003@hotmail.com>
2024-10-17 20:40:37 -0600

    :construction: breakpoint

--- app/services/openai_service.py
+++ app/services/openai_service.py
@@ -1,8 +1,8 @@
 # app/services/openai_service.py
 import os
-from app.actions.student_result_action import obtener_resultado_estudiante, crear_prompt_alumno
 from openai import OpenAI
 from dotenv import load_dotenv
+from app.services.action_handle_service import ActionHandleService
 
 load_dotenv(override=True)
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
@@ -10,35 +10,21 @@ client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 class OpenAIService:
 
     def __init__(self):
-        pass
+        self.action_handle_service = ActionHandleService()
 
     def handle_request(self, prompt):
         try:
-            # Imprimir el prompt del usuario
-            print(f"Usuario: {prompt}")
+            # Manejar la solicitud con ActionHandleService
+            action_response = self.action_handle_service.handle_request(prompt)
 
-            # Si el prompt solicita el estado de un alumno
-            if "estado de" in prompt.lower():
-                # Extraer el nombre o email del prompt
-                name_or_email = prompt.split('estado de')[-1].strip()
+            if action_response.get("status") == "error":
+                return action_response
 
-                # Obtener la información del estudiante desde la acción
-                alumno_info = obtener_resultado_estudiante(name_or_email)
+            gpt_prompt = action_response.get("prompt")
 
-                # Verificar si se encontró al estudiante
-                if alumno_info:
-                    # Crear el mensaje para GPT basándose en la información del alumno
-                    gpt_prompt = crear_prompt_alumno(alumno_info)
-
-                    # Enviar el mensaje a GPT para análisis
-                    gpt_response = self._enviar_a_gpt(gpt_prompt)
-                    return {"status": "success", "response": gpt_response}
-                else:
-                    return {"status": "error", "response": f"No se encontró información del alumno '{name_or_email}'."}
-            else:
-                # Prompt general, enviar directamente a GPT
-                gpt_response = self._enviar_a_gpt(prompt)
-                return {"status": "success", "response": gpt_response}
+            # Enviar el prompt a OpenAI
+            gpt_response = self._enviar_a_gpt(gpt_prompt)
+            return {"status": "success", "response": gpt_response}
         
         except Exception as e:
             return {"status": "error", "response": str(e)}
